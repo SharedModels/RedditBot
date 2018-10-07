b2f88deb70a1fbed14601b23e60a09fa2988943d
B2B\dysont <tom.dyson@reedbusiness.com>
2018-10-07 20:05:21 +0100

    slight changes

--- dashboard_scrape_draw.py
+++ dashboard_scrape_draw.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
     from subreddit_relations import SubredditRelations
     import pandas as pd
     relations = SubredditRelations()
-    subreddit_list = relations.top_100_subreddits(11)
+    subreddit_list = relations.top_100_subreddits(1)
     relations_dict = relations.find_relations(subreddit_list)
     relations_df = pd.DataFrame(relations_dict).fillna(0)
+    print(relations.comment_upvote)
     network_graph = DrawNetworkGraph()
     network_graph.dashboard_graph(relations_df)
--- subreddit_relations.py
+++ subreddit_relations.py
@@ -22,7 +22,7 @@ class SubredditRelations():
         self.global_user_list = []
         self.global_user_counts = {}
         self.relations_df_list = []
-        self.comment_upvote = []
+        self.comment_upvote = {}
 
     def retrieve_submission_comments(self, submission):
         self.subreddit_list.append(submission.comments.list())
@@ -36,13 +36,13 @@ class SubredditRelations():
 
     def retrieve_users(self, subred):
         user_list = []
-        upvote_comment_dict = {}
+        upvote_comment_dict = {'comment' : [], 'upvote' : []}
         for comment_list in self.comments_dict[subred]:
             for comment in comment_list:
                 try:
                     user_list.append(comment.author.name)
-                    upvote_comment_dict['commenet'] = comment.body
-                    upvote_comment_dict['upvote'] = comment.score
+                    upvote_comment_dict['comment'].append(comment.body)
+                    upvote_comment_dict['upvote'].append(comment.score)
                     # TODO comments and upvotes
                 except Exception as e:
                     # print(e)
