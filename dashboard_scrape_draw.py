c3725d9426d62e9a803f51c496a60e7a73609d11
B2B\dysont <tom.dyson@reedbusiness.com>
2018-10-07 19:56:06 +0100

    combine script

---
+++ dashboard_scrape_draw.py
@@ -0,0 +1,15 @@
+from network_graph import DrawNetworkGraph
+from subreddit_relations import SubredditRelations
+import pandas as pd
+
+
+if __name__ == '__main__':
+    from network_graph import DrawNetworkGraph
+    from subreddit_relations import SubredditRelations
+    import pandas as pd
+    relations = SubredditRelations()
+    subreddit_list = relations.top_100_subreddits(11)
+    relations_dict = relations.find_relations(subreddit_list)
+    relations_df = pd.DataFrame(relations_dict).fillna(0)
+    network_graph = DrawNetworkGraph()
+    network_graph.dashboard_graph(relations_df)
